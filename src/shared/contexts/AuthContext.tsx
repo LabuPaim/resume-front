804a412330fb6ddeb8851a175d36bdf4c3be8765
LabuPaim <victorpaim80@gmail.com>
2023-02-11 01:00:47 -0300

    agora vai em

--- src/shared/contexts/AuthContext.tsx
+++ src/shared/contexts/AuthContext.tsx
@@ -22,12 +22,11 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   }
 
   useEffect(() => {
-
     console.log(localStorage.getItem("user"));
     const user = JSON.parse(localStorage.getItem("user") ?? "");
-    console.log("to aqui pai",user);
-    const token = localStorage.getItem("token")
-    if (user && token) setUserRequest({ token, user });
+    console.log("to aqui pai", user);
+    const token = localStorage.getItem("token");
+    if (token) setUserRequest({ token, user });
   }, []);
 
   return (
